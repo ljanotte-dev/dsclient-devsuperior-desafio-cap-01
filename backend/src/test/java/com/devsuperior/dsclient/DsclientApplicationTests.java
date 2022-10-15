2e4d042f08b4dd81784a22355907bb3da9c1258b
ljanotte-dev <ljanotte-dev@gmail.com>
2022-10-15 16:16:58 -0300

    Backend project created

---
+++ backend/src/test/java/com/devsuperior/dsclient/DsclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
+
+@SpringBootConfiguration
+class DsclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
